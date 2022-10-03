a9f5770de2658d59522061ebdbf74fcba8b8c873
Marcela Fróes <91344634+gottmik@users.noreply.github.com>
2022-10-03 18:55:36 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Content from './components/container/content';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <Content/>
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/container/content.jsx
@@ -0,0 +1,20 @@
+import './content.css';
+import React from 'react';
+
+function Content() {
+  return (
+    <div className="content">
+      <form action="">
+        <div className='caixa'>
+          <input type='text' required/>
+          <span>Altura</span>
+        </div>
+
+      </form>
+      
+      
+    </div>
+  );
+}
+
+export default Content;
